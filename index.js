b59ec4fc8a7e4d26de1dcfe95e20828df911c5a1
MD Rajib <83833885+blackweb71@users.noreply.github.com>
2021-11-18 12:31:22 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+x = document.querySelector("title");
+a = document.querySelector(".but1");
+b = document.querySelector(".but2");
+c = document.querySelector(".but3");
+d = document.querySelector(".but4");
+e = document.querySelector(".but5");
+f = document.querySelector(".but6");
+
+
+x.innerHTML="Villain";
+a.innerHTML="Venom 2";
+b.innerHTML="Squid Game";
+c.innerHTML="Space Jam: A New Legacy";
+d.innerHTML="Free Guy";
+e.innerHTML="Shang Chi";
+f.innerHTML="Wish Dragon";
+
+
+
