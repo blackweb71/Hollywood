deb1a9a9919c93f6d1629c374d6511937fb697b8
MD Rajib <83833885+blackweb71@users.noreply.github.com>
2021-11-19 13:35:06 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ c = document.querySelector(".but3");
 d = document.querySelector(".but4");
 e = document.querySelector(".but5");
 f = document.querySelector(".but6");
+h = document.querySelector(".but8");
 
 
 x.innerHTML="Villain";
@@ -14,6 +15,7 @@ c.innerHTML="Space Jam: A New Legacy";
 d.innerHTML="Free Guy";
 e.innerHTML="Shang Chi";
 f.innerHTML="Wish Dragon";
+h.innerHTML="RED NOTICE";
 
 
 
